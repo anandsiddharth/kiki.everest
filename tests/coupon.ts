e9b82b2f8d195644b5003cf955ca395c20a9792d
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-11 01:42:09 +0530

    add find function

--- tests/coupon.ts
+++ tests/coupon.ts
@@ -1,12 +1,24 @@
 const assert = require("assert");
+
 import { Coupon } from "../src/modules/coupon/coupon";
 
 
 
 describe('Coupon', function () {
-    describe('try to create coupon instance with coupon code', function () {
+    describe('try to create coupon instance', function () {
+
         it('should throw exception when code is not present', function () {
             assert.throws(() => Coupon.fromJSON({ code: "", validations: [] }), Error, "Error thrown");
         });
+        it('should create coupon instance', function () {
+            assert.ok(Coupon.fromJSON({ code: "OFFR1", validations: [] }));
+        });
+        it('should find coupon OFR001', function () {
+            assert.ok(Coupon.find("OFR001"));
+        });
+        it('should return null', function () {
+            assert.equal(Coupon.find("OFR0012"), null);
+        });
+
     });
 });
