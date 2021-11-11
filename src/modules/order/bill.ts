6a6443749f67aa81bc7c7d36f06e6013efdd509f
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-11 09:16:05 +0530

    billing function

---
+++ src/modules/order/bill.ts
@@ -0,0 +1,16 @@
+import { apply } from '../coupon/apply'
+import { Coupon } from '../coupon/coupon'
+import { Order } from './order'
+export const bill = (baseDeliveryCost: number, order: Order): Order => {
+    order.cost = baseDeliveryCost + (order.weight * 10) + (order.distance * 5)
+    order.discount = 0
+
+    if (order.couponCode && order.couponCode.length) {
+        order.discount = apply(order, Coupon.find(order.couponCode))
+    }
+
+
+    order.total = order.cost - order.discount
+
+    return order
+}
