98587247b1b7367089e8cbb3da0764bc3ae73b5d
Anand Siddharth <anand.siddharth@hotmail.com>
2021-11-11 09:13:31 +0530

    add find function

--- src/modules/coupon/coupon.ts
+++ src/modules/coupon/coupon.ts
@@ -1,15 +1,29 @@
-import { ValidationRule } from "./validation";
+
+import { Discount } from "./discount";
+import { load } from "./load";
+import { Validation } from "./validation";
 export class Coupon {
     public code: string
-    public validations: ValidationRule[]
+    public discount: Discount
+    public validations: Validation[]
 
-    constructor(code: any, validations: Array<any>) {
-        if (!code || !code.length) throw new Error('coupon code is not valid')
+    constructor(code: any, discount: any, validations: Array<any>) {
+        if (!code || !code.length) throw new Error('[InvalidInput] coupon code is not valid')
         this.code = code;
         this.validations = validations;
+        this.discount = discount
     }
 
     public static fromJSON(json: any): Coupon {
-        return new Coupon(json.code, json.validations);
+        return new Coupon(json.code, json.discount, json.validations);
     }
+
+    public static find(code: string): Coupon | null {
+        let coupon = load().filter((x: any) => x.code == code)[0];
+        if (coupon) {
+            return Coupon.fromJSON(coupon);
+        }
+        return null
+    }
+
 }
